e32d448be7415a15a857d9a0d6773b604d70d156
Mai <garcia.ton94@gmail.com>
2023-05-02 21:01:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching (drivers, name) {
+    const newDrivers = drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    });
+
+    return newDrivers;
+}
+
+function fuzzyMatch (drivers, name) {
+    const newDrivers = drivers.filter(function (driver) {
+        return driver.charAt(0) === name.charAt(0);
+    });
+    return newDrivers;
+}
+
+function matchName (drivers, name) {
+    const newDrivers = drivers.filter(function (driver) {
+        return driver.name === name; 
+    });
+    return newDrivers;
+} 
+
